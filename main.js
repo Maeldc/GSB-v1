508c9c15b57cbe0a8928d311ec5c9db058c08309
Maeldc <47451253+Maeldc@users.noreply.github.com>
2021-02-06 00:02:26 +0100

    first commit
    first commit

---
+++ main.js
@@ -0,0 +1,104 @@
+const button = document.querySelector('.button_connect');
+console.log(button);
+
+let mail;
+/*
+button.addEventListener('click', () => {
+    
+    mail = document.querySelector('#username').value;
+    if(mail === "mae.lodico@gmail.com"){
+
+    }
+
+
+    alert('vous avez mis'+mail);
+    console.log(mail);
+
+});
+
+/* const equipes = document.querySelectorAll('ul li');
+
+//equipes.style.display = "none";
+console.log(equipes);
+equipes.forEach((equipe) => {
+    if(equipe.innerText === "Leeds"){
+        equipe.style.display = "none";
+    }
+});
+
+const img = document.querySelector('img');
+
+/* setTimeout(() => {
+    img.style.borderRadius = "50%";
+}, 2000); 
+
+setTimeout(() => {
+    img.classList.add('circle');
+}, 2000); */
+
+///////// recuperer la valur d'un formulaire //////////////
+
+/* const userAge = document.querySelector('#user_input');
+
+console.log(parseInt(userAge.value));
+
+const img = document.querySelector('img');
+const nvlImg = document.querySelector('#sun'); */
+
+/* nvlImg.addEventListener(TYPEEVENT, CALLBACK) */
+
+/* img.addEventListener('click', () => {
+    img.classList.toggle('circle');
+}); */
+
+/* 
+var validClick = 0;
+img.addEventListener('click', () => {
+    if(validClick === 0){
+        validClick = 1;
+        img.classList.add('circle');
+    } else {
+        validClick = 0;
+        img.classList.remove('circle');
+    }
+}); */
+
+
+/*
+const bodyPage = document.querySelector('body');
+const r = document.querySelector('#rouge');
+const b = document.querySelector('#bleu');
+const v = document.querySelector('#vert');
+
+
+r.addEventListener('mouseover', () => {
+
+    bodyPage.classList.add("rouge");    
+    bodyPage.classList.remove("vert");    
+    bodyPage.classList.remove("bleu");    
+
+});
+
+b.addEventListener('mouseover', () => {
+
+    bodyPage.classList.add("bleu");   
+    bodyPage.classList.remove("vert");    
+    bodyPage.classList.remove("rouge");   
+
+});
+v.addEventListener('mouseover', () => {
+
+    bodyPage.classList.add("vert");   
+    bodyPage.classList.remove("rouge");    
+    bodyPage.classList.remove("bleu");   
+
+});
+
+
+/////////// AUTRE POSSIBILITE ////////
+/* const affichage = () =>{
+    alert('eh Mercééééé');
+}
+ 
+nvlImg .addEventListener('click', affichage);*/
+////////////////////////////////////////////
