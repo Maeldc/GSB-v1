112e5a362d72ba1ffe0bed4e90bf3c86b727648f
Maeldc <47451253+Maeldc@users.noreply.github.com>
2021-03-01 18:52:45 +0100

    requete http

--- cartes/flight.js
+++ cartes/flight.js
@@ -71,7 +71,53 @@ const DataPraticien = [
   }
 ];
 
-    const list = document.querySelector('.cartes_praticiens');
+const url = "http://localhost:90/gsb/praticien";
+
+
+//Afficher dans la console tous les praticiens de Marseille
+
+fetch(url + "?ville=Marseille")
+.then(response => response.json())
+.then((datas) => {
+    datas.forEach( (element)=> {
+        console.log('praticiens de Marseille => [' +element.nom+' => '+element.adresse);
+    });
+});
+
+
+//Afficher dans la console tous les dentistes de Paris
+
+fetch(url + "?ville=Paris&specialite=Dentiste")
+.then(response => response.json())
+.then((datas) => {
+    datas.forEach( (element)=> {
+        console.log('dentistes de Paris => [' +element.nom+', '+ element.adresse +' '+element.specialite+']');
+    });
+});
+
+
+//Afficher dans la console tous les praticiens qui ont été visité
+
+fetch(url + "?visite=1")
+.then(response => response.json())
+.then((datas) => {
+    datas.forEach( (element)=> {
+        console.log('praticiens qui ont été visité => [' +element.nom+' => '+element.specialite+']');
+    });
+});
+
+
+//Afficher dans la console tous les dentistes de Marseille qui ont été visité
+  
+fetch(url + "?ville=Marseill&specialite=Dentiste&visite=1")
+.then(response => response.json())
+.then((datas) => {
+    datas.forEach( (element)=> {
+        console.log('dentistes de Marseille qui ont été visité => [' +element.nom+' => '+element.specialite+']');
+    });
+});
+
+ /*   const list = document.querySelector('.cartes_praticiens');
     DataPraticien.forEach((datas) => {
         console.log(datas.nom);
         console.log(datas.prenom);
@@ -79,32 +125,7 @@ const DataPraticien = [
         console.log(datas.specialite);
 
         list.insertAdjacentHTML('beforeend', `
-            <div class="bloc_praticien">
-              <div class="bloc_actions">
-              <img class="update" src="3592815-compose-create-edit-edit-file-office-pencil-writing_107734.png" alt="Modifier un praticien">
-                <div id="myModal" class="modal">
-                  <div class="modal-content">
-                    <span class="close"><img class="exit" src="clear.png" alt="quitter"></span>
-                    <br>
-                    <p></p>
-                  </div>
-                </div>
-                <img class="delete" src="delete.png" alt="Supprimer un praticien">
-              </div>
-              <div class="bloc_name">
-                <p><b>Dr. ${datas.nom} ${datas.prenom}</b></p>
-              </div>
-              <div class="bloc_specialite">
-                <p>${datas.specialite}</p>
-              </div>
-              <div id="space2">
-                &nbsp;
-              </div>
-              <div class="bloc_location">
-                <img class="location" src="japan.png" alt="${datas.adresse}">
-                <p>à ${datas.adresse}</p>
-              </div>
-            </div> 
+            
         `);
         // je recupere le modal
         let modal = document.querySelector("#myModal");
@@ -128,7 +149,7 @@ const DataPraticien = [
         });
         
     });
-
+*/
     
 
 
